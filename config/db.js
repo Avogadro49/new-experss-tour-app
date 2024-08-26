706915590a2de7a5db087860c4d59f36211bd192
Avogadro49 <tedozashvilinika@gmail.com>
2024-08-26 12:59:08 +0400

    append import/delete functionality

--- config/db.js
+++ config/db.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");
+const Tour = require("../model/TourModel");
+
+dotenv.config({ path: path.join(__dirname, "config.env") });
 
 const connectDB = async () => {
   try {
@@ -10,4 +16,41 @@ const connectDB = async () => {
   }
 };
 
+//read json file
+//prettier-ignore
+const tours = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/tours.json'), 'utf-8'));
+
+//IMPORTING data to db
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("data successfully imported");
+    process.exit();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+//DELETING data to db
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("data successfully deleted");
+    process.exit();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "--import" || process.argv[2] === "--delete") {
+  connectDB().then(() => {
+    if (process.argv[2] === "--import") {
+      importData();
+    } else if (process.argv[2] === "--delete") {
+      deleteData();
+    }
+  });
+}
 module.exports = connectDB;
